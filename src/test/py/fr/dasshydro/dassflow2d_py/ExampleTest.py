d425f69e7ec24684693fe6b7a2a49630e42a63c8
loferga <laforgemateo@gmail.com>
2025-07-16 15:06:11 +0200

    Adds primitive tests as placeholder and move files
    this commit modify project structure to implement basic tests placeholder,
    this will be used to create a CI/CD pipeline early in development

---
+++ src/test/py/fr/dasshydro/dassflow2d_py/ExampleTest.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+from io import StringIO
+from src.main.py.fr.dasshydro.dassflow2d_py.Example import example
+
+class TestExample(unittest.TestCase):
+
+    def test_example(self):
+        # redirect stdout to capture the print statement
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        example()
+
+        # reset stdout
+        sys.stdout = sys.__stdout__
+
+        # check retrieved value
+        self.assertEqual(captured_output.getvalue().strip(), "Hello World!")
+
+if __name__ == '__main__':
+    unittest.main()
