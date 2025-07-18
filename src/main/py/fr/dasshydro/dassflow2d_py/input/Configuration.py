1b85c80e85b73b7a558d245735e42ad3ed29bcc9
loferga <laforgemateo@gmail.com>
2025-07-18 17:13:47 +0200

    Adds barebone configuration reading
    create placeholder for configuration readings,
    and enhance shallow water runner

---
+++ src/main/py/fr/dasshydro/dassflow2d_py/input/Configuration.py
@@ -0,0 +1,9 @@
+# imports
+
+class Configuration:
+
+    def __init__(self):
+        raise NotImplementedError("Not yet implemented.")
+
+def load_from_file(file_path: str) -> Configuration:
+    raise NotImplementedError("Not yet implemented.")
--- src/main/py/fr/dasshydro/dassflow2d_py/run_shallow_water_model.py
+++ src/main/py/fr/dasshydro/dassflow2d_py/run_shallow_water_model.py
@@ -1,28 +1,31 @@
 # imports
-# ...
+import input.Configuration as cfg
 
-def run_shallow_water_model(input_folder_path: str):
+def run_shallow_water_model(input_folder_path: str, configuration_file: str):
     """
     Configure and run the shallow water model using data from the specified input folder.
 
     This function is responsible for setting up any necessary data required for the shallow water model
     and then executing the model using the configured data.
 
-    :param str input_folder_path: The path to the folder containing the input files for the shallow water model. \
+    :param str input_folder_path: The path to the folder containing the input files for the shallow water model.
         This folder is assumed to contains all informations necessary for the shallow water model to run
+    :param str configuration_file: The name of the file inside the input folder path that represent the configuration file.
     :raises: NotImplementedError: This function is not implemented yet and will raise a NotImplementedError when called.
     """
-    raise NotImplementedError("the run_shallow_water_model routine has not been implemented yet.")
+    configuration = cfg.load_from_file(f"{input_folder_path}/{configuration_file}")
+    raise NotImplementedError("Not yet implemented.")
 
 import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Process a folder path")
     parser.add_argument("folder_path", type=str, help="Path to the folder containing input files")
+    parser.add_argument("--configuration-file", "-c", type=str, help="Configuration file name", default="input.txt", required=False)
 
     args = parser.parse_args()
     input_folder_path = args.folder_path
-    run_shallow_water_model(input_folder_path)
+    run_shallow_water_model(input_folder_path, args.configuration_file)
 
 if __name__ == "__main__":
     main()
