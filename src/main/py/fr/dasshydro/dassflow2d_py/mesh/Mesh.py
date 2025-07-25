430a30fc3b3f5f69d723e5e2f84430b615f3b299
abou0117 <sidikibah100@gmail.com>
2025-07-25 18:13:12 +0200

    Adds mesh structure from spec
    Use formal specification to bring mesh structure as abstract classes.
    Also allows to hold documentation for users

--- src/main/py/fr/dasshydro/dassflow2d_py/mesh/Mesh.py
+++ src/main/py/fr/dasshydro/dassflow2d_py/mesh/Mesh.py
@@ -1,4 +1,9 @@
 from enum import Enum
+from abc import ABC, abstractmethod
+from mesh.Node import Node
+from mesh.Edge import Edge
+from mesh.Cell import Cell
+from mesh.BoundaryBehavior import BoundaryBehavior  
 
 class MeshShape(Enum):
     TRIANGULAR = "triangular"
@@ -9,7 +14,100 @@ class MeshType(Enum):
     BASIC = "basic"
     DASSFLOW = "dassflow"
 
-class Mesh:
 
-    def __init__(self):
-        raise NotImplementedError("Not yet implemented.")
+class Mesh(ABC):
+
+    @abstractmethod
+    def get_cells(self) -> list[Cell]:
+        """Get the list of cells in the mesh.
+
+        :return: List of cells.
+        :rtype: list[Cell]
+        """
+        pass
+    @abstractmethod
+    def get_cell_number(self) -> int:
+        """Get the number of cells in the mesh.
+
+        :return: Number of cells.
+        :rtype: int
+        """
+        pass
+
+    @abstractmethod
+    def get_boundary_cell_behaviors(self) -> list[BoundaryBehavior[Cell]]:
+        """Get the list of boundary behaviors associated with cells in the mesh.
+
+        :return: List of boundary behaviors.
+        :rtype: list[BoundaryBehavior[Cell]]
+        """
+        pass
+
+    @abstractmethod
+    def get_boundary_cell_behavior_number(self) -> int:
+        """Get the number of boundary behaviors associated with cells in the mesh.
+
+        :return: Number of boundary behaviors.
+        :rtype: int
+        """
+        pass
+
+    @abstractmethod
+    def get_edges(self) -> list[Edge]:
+        """Get the list of edges in the mesh.
+
+        :return: List of edges.
+        :rtype: list[Edge]
+        """
+        pass
+    @abstractmethod
+    def get_edge_number(self) -> int:
+        """Get the number of edges in the mesh.
+
+        :return: Number of edges.
+        :rtype: int
+        """
+        pass
+    @abstractmethod
+    def get_boundary_edge_behaviors(self) -> list[BoundaryBehavior[Edge]]:
+        """Get the list of boundary behaviors associated with edges in the mesh.
+
+        :return: List of boundary behaviors.
+        :rtype: list[BoundaryBehavior[Edge]]
+        """
+        pass
+
+    @abstractmethod
+    def get_boundary_edge_behavior_number(self) -> int:
+        """Get the number of boundary behaviors associated with edges in the mesh.
+
+        :return: Number of boundary behaviors.
+        :rtype: int
+        """
+        pass
+
+    @abstractmethod
+    def get_nodes(self) -> list[Node]:
+        """Get the list of nodes in the mesh.
+
+        :return: List of nodes.
+        :rtype: list[Node]
+        """
+        pass
+
+    @abstractmethod
+    def get_node_number(self) -> int:
+        """Get the number of nodes in the mesh.
+
+        :return: Number of nodes.
+        :rtype: int
+        """
+        pass
+    @abstractmethod
+    def get_surface(self) -> float:
+        """Get the total surface area of the mesh.
+
+        :return: Total surface area.
+        :rtype: float
+        """
+        pass
