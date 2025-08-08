a48b24b4f36889a0cd75a0ba3772afd55995f273
loferga <laforgemateo@gmail.com>
2025-08-08 17:24:22 +0200

    Make configuration structure compliant with diagram
    this commit sort, adds and filter the getters needed for the configuration,
    according to the identified parameters needed by the program

--- src/main/py/fr/dasshydro/dassflow2d_py/input/Configuration.py
+++ src/main/py/fr/dasshydro/dassflow2d_py/input/Configuration.py
@@ -1,18 +1,28 @@
-from fr.dasshydro.dassflow2d_py.mesh.Mesh import MeshType, MeshShape
 from fr.dasshydro.dassflow2d_py.resolution.ResolutionMethod import TemporalScheme, SpatialScheme
 
 class Configuration:
+    """
+    This class holds all configuration namespaces and link them to a corresponding getter.
+    This class does not support data source resolution
+    (i.e. it does not remember for each namespace, the source of the associated value)
+    """
+
+    # Define default values and valid namespaces at the same time
+    DEFAULT = {
+        'temporal-scheme': 'euler',
+        'spatial-scheme': 'first',
+        'mesh-file': 'mesh.geo',
+        'initial-state-file': 'dof_init.txt',
+        'bathymetry-file': 'bathymetry.txt',
+        'manning-file': 'manning.txt',
+        'result-path': 'output/',
+        'simulation-time': '10000',
+        'delta-to-write': '100',
+        'is-delta-adaptative': 'False',
+        'default-delta': '0.01'
+    }
 
     def __init__(self):
-        raise NotImplementedError("Not yet implemented.")
-
-    def getMeshType(self) -> MeshType:
-        raise NotImplementedError("Not yet implemented.")
-
-    def getMeshShape(self) -> MeshShape:
-        raise NotImplementedError("Not yet implemented.")
-    
-    def getMeshFile(self) -> str:
         raise NotImplementedError("Not yet implemented.")
 
     def getTemporalScheme(self) -> TemporalScheme:
@@ -21,22 +31,31 @@ class Configuration:
     def getSpatialScheme(self) -> SpatialScheme:
         raise NotImplementedError("Not yet implemented.")
     
+    def getMeshFile(self) -> str:
+        raise NotImplementedError("Not yet implemented.")
+    
     def getInitialStateFile(self) -> str:
+        raise NotImplementedError("Not yet implemented.")
+
+    def getBathymetryFile(self) -> str:
+        raise NotImplementedError("Not yet implemented.")
+
+    def getManningFile(self) -> str:
+        raise NotImplementedError("Not yet implemented.")
+    
+    def getResultFilePath(self) -> str:
         raise NotImplementedError("Not yet implemented.")
     
     def getSimulationTime(self) -> float:
         raise NotImplementedError("Not yet implemented.")
     
-    def isDeltaAdaptative(self) -> bool:
-        raise NotImplementedError("Not yet implemented.")
-    
-    def getDelta(self) -> float:
-        raise NotImplementedError("Not yet implemented.")
-    
     def getDeltaToWrite(self) -> float:
         raise NotImplementedError("Not yet implemented.")
     
-    def getResultFilePath(self) -> str:
+    def isDeltaAdaptative(self) -> bool:
+        raise NotImplementedError("Not yet implemented.")
+    
+    def getDefaultDelta(self) -> float:
         raise NotImplementedError("Not yet implemented.")
     
     def updateValues(self, values: dict[str, str]):
