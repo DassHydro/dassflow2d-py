204be262c07c299b4f28b640a8013669d4e7014f
loferga <laforgemateo@gmail.com>
2025-08-25 14:29:08 +0200

    Implements TimeStepState as presented in the diagram
    authentically replicate time step state structure as presented in the class diagram

--- src/main/py/fr/dasshydro/dassflow2d_py/d2dtime/TimeStepState.py
+++ src/main/py/fr/dasshydro/dassflow2d_py/d2dtime/TimeStepState.py
@@ -1,4 +1,17 @@
+from fr.dasshydro.dassflow2d_py.mesh.Mesh import Cell
+
+class Node:
+
+    def __init__(self, h: float, u: float, v: float):
+        self.h = h
+        self.u = u
+        self.v = v
+
+
 class TimeStepState:
 
-    def __init__(self):
-        raise NotImplementedError("Not yet implemented.")
+    def __init__(self, state: dict[Cell, Node]):
+        self.state = state
+
+    def getNode(self, cell: Cell) -> Node:
+        return self.state[cell]
