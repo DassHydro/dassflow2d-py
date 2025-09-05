ae7dd4c1ab1044b8eb94427a8c3ca4389c915d5c
loferga <laforgemateo@gmail.com>
2025-09-05 10:55:12 +0200

    Fix sw runner type mispatch
    modify shallow water runner to integrate mesh creation

--- src/main/py/fr/dasshydro/dassflow2d_py/input/MeshReader.py
+++ src/main/py/fr/dasshydro/dassflow2d_py/input/MeshReader.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
-from fr.dasshydro.dassflow2d_py.mesh.Mesh import RawCell, RawVertex
+from fr.dasshydro.dassflow2d_py.mesh.Mesh import RawCell, RawVertex, RawInlet, RawOutlet
 
 class MeshReader(ABC):
     @abstractmethod
-    def read(self, file_path: str) -> tuple[list[RawVertex], list[RawCell], list[int], list[int]]:
+    def read(self, file_path: str) -> tuple[list[RawVertex], list[RawCell], list[RawInlet], list[RawOutlet]]:
         pass
